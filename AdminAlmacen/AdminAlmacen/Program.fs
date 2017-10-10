772022e4182c68dab664764ccb43ce2209bb32d8
Carlos <30960256+CarlosRox@users.noreply.github.com>
2017-10-09 19:18:20 -0500

    Update Program.fs

--- AdminAlmacen/AdminAlmacen/Program.fs
+++ AdminAlmacen/AdminAlmacen/Program.fs
@@ -1,7 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 open System
 
-[<EntryPoint>]
-let main argv = 
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let array1 = [| 1; 2; 3; 4; 5; 6 |]
+    for i in 0 .. array1.Length - 1 do
+    printf "%d " array1.[i]
+    printfn" "
+    
+    let array2 = [|
+      1
+      2
+      3
+      4
+      5
+   |]
+for i in 0 .. array2.Length - 1 do
+   printf "%d " array2.[i]
+printfn" "
+
+let array3 = [| for i in 1 .. 10 -> i * i |]
+for i in 0 .. array3.Length - 1 do
+   printf "%d " array3.[i]
+printfn" "
+
+Console.ReadLine()
